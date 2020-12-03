cf4b61bd5f3dea847513f0805f368a2dff782d1f
annesteenbeek <annesteenbeek@gmailcom>
2020-12-03 20:51:47 +0100

    requirement fixes, generate script

--- generate_server_models.sh
+++ generate_server_models.sh
@@ -16,50 +16,70 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # make sure cwd is root dir
 cd "$DIR"
 
+if [ ! -d "data/lp-detector" ]; then
+  bash get-networks.sh
+fi
+
 # ----- YOLOv3-SPP ----- (vehicle detection)
 echo "Setting up YOLOv3-SPP"
-git clone https://github.com/mystic123/tensorflow-yolo-v3.git
-mkdir -p data/yolov3-spp
-cd data/yolov3-spp
-echo "Getting yolov3 weights"
-wget https://pjreddie.com/media/files/yolov3-spp.weights 
+if [ ! -d "tensorflow-yolo-v3" ]; then
+  git clone https://github.com/mystic123/tensorflow-yolo-v3.git
+fi
 
-# Use this repo to convert darknet to tensorflow
-echo "Converting yolov3 weights to tensorflow .pb"
-python ../../tensorflow-yolo-v3/convert_weights_pb.py \
-  --class_names coco.names \
-  --data_format NHWC \
-  --weights_file yolov3-spp.weights \
-  --spp 
+if [ -f "data/yolov3-spp/yolov3-spp.weights" ]; then
+  echo "weights already exists, skipping"
+else
+  mkdir -p data/yolov3-spp
+  echo "Getting yolov3 weights"
+  wget https://pjreddie.com/media/files/yolov3-spp.weights -P data/yolov3-spp
+  wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names -P data/yolov3-spp
+fi
 
-cd "$DIR"
-# build server side model
-python tools/servable/yolov3-spp.py \
-  data/yolov3-spp/frozen_darknet_yolov3_model.pb \
-  data/yolov3-spp/1 \
-  data/yolov3-spp/coco.names
+if [ ! -d "data/yolov3-spp/1" ]; then
+  echo "Converting yolov3 weights to tensorflow .pb"
+  cd data/yolov3-spp
+  python ../../tensorflow-yolo-v3/convert_weights_pb.py \
+    --class_names coco.names \
+    --data_format NHWC \
+    --weights_file yolov3-spp.weights \
+    --spp 
+
+  cd "$DIR"
+  # build server side model
+  python tools/servable/yolov3-spp.py \
+    data/yolov3-spp/frozen_darknet_yolov3_model.pb \
+    data/yolov3-spp/1 \
+    data/yolov3-spp/coco.names
+fi
 
 # ---- WPOD-Net ------ (License plate detection)
-python tools/servable/wpod-net.py \
-  data/lp-detector/wpod-net_update1 \
-  data/lp-detector/1
+if [ ! -d "data/lp-detector/1" ]; then
+  python tools/servable/wpod-net.py \
+    data/lp-detector/wpod-net_update1 \
+    data/lp-detector/1
+fi
 
 # ---- OCR-Net ------- (OCR)
 # install darkflow 
-git clone https://github.com/thtrieu/darkflow.git
-cd darkflow
-python setup.py build_ext --inplace
-cd ../data/
+if [ ! -d "darkflow" ]; then
+  git clone https://github.com/thtrieu/darkflow.git
+  cd darkflow
+  python setup.py build_ext --inplace
+  cd ..
+fi
 
-./../darflow/flow \
-  --model ocr/ocr-net.cfg \
-  --load ocr/ocr-net.weights \
-  --labels ocr/ocr-net.names \
-  --savepb
+if [ ! -d "data/ocr/1" ]; then
+  cd data/
+  ./../darflow/flow \
+    --model ocr/ocr-net.cfg \
+    --load ocr/ocr-net.weights \
+    --labels ocr/ocr-net.names \
+    --savepb
 
-# Convert ocr-net to tensorflow servable
-cd "$DIR"
-python tools/servable/ocr-net.py \
-  darkflow/built_graph/ocr-net.pb \
-  data/ocr/1 \
-  data/ocr/ocr-net.names
+  # Convert ocr-net to tensorflow servable
+  cd "$DIR"
+  python tools/servable/ocr-net.py \
+    darkflow/built_graph/ocr-net.pb \
+    data/ocr/1 \
+    data/ocr/ocr-net.names
+fi
